e89093d5fe5f4490bac69b34e7c866cdf7ddcf85
mksameera89 <mksameera@gmail.com>
2023-07-05 16:03:06 +0530

    adding database connection details

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ const image = require('./Controllers/image');
 const db = knex({
                     client: 'pg',
                     connection: {
-                    host : 'dpg-cidek8tph6eounfdk970-a.oregon-postgres.render.com',
-                    port : 5432,
-                    user : 'root',
-                    password : 'HvgzOlpChQ7SX7wVf8UnI28c3XYnxArf',
-                    database : 'smartbraindb_o8vf'
+                        connectionString : 'postgres://smartbraindb_gokn_user:NbLEsavMy6zPhpTTMeM8awQMYKPN1LKa@dpg-ciijidlph6erq6h28ng0-a/smartbraindb_gokn',
+                        ssl: {rejectUnauthorized: false},
+                        host : 'dpg-cidek8tph6eounfdk970-a.oregon-postgres.render.com',
+                        port : 5432,
+                        user : 'root',
+                        password : 'HvgzOlpChQ7SX7wVf8UnI28c3XYnxArf',
+                        database : 'smartbraindb_o8vf'
                     }
                 });
   
